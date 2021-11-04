8ee11bed61f453fc99eb9205451c47162e9b9236
buronpro <buronbek33@gmail.com>
2021-11-04 19:54:43 +0500

    git clon

---
+++ main.js
@@ -0,0 +1,10 @@
+var home = document.querySelector (".home");
+
+var Sutedint = prompt ("Qaysi oyda odamlar kam gapiradilar?  Iltimos mayda harifda yozing!");
+
+var hotel = "fevral";
+
+if ( Sutedint == hotel  ) 
+alert ("siz yudingiz:🥳: Fevral oyida. Negaki bu oyda kunlar kam") ;
+
+else ( home.textContent = "JAVOB XATO:😔 QAYTA O'RINIB KURING" );
